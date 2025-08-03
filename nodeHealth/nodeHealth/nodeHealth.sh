e596adb80a729f06b3dde8d2aa40160e8f2ae056
korzengwiezdny <mariuszf.urban@gmail.com>
2025-08-03 20:39:16 +0200

    nodeHealth v2

--- nodeHealth/nodeHealth/nodeHealth.sh
+++ nodeHealth/nodeHealth/nodeHealth.sh
@@ -10,6 +10,10 @@
 #
 #Version: v1
 #
+#About: We can monitor health our server Linux or cloud instance. Its basic information about a system.
+#
+#How to use: Change permission in the file, and execute ~/nodeHealth.sh, we can put this script to crontab.
+#
 ############################################
 
 set -x  # debug mode
@@ -17,10 +21,11 @@ set -e # exit the script when there is an error
 set -o pipefail
 
 
-df -h
+df -h #printing a size volumin
 
-free -g
+free -g # printing free ram
 
-nproc
+nproc # number of logic cores
 
-ps -ef | grep docker | awk -F" " '{print $2}' 
+ps -ef | grep docker | awk -F" " '{print $2}' # Looking for ID of proces for e.g. docker
+
